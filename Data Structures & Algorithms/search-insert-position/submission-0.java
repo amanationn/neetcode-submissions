fb6f1cb05794c184385c12e90139c817ea6b0b9f
Aman Gupta <81767337+amanationn@users.noreply.github.com>
2026-07-09 01:15:58 +0530

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            if(nums[mid] < target) {
+                l = mid + 1;
+            }
+            else {
+                if(nums[mid] == target) {
+                    while(mid >= 0 && nums[mid] == target)
+                        mid -= 1;
+                    return mid + 1;
+                }
+                if(mid == 0 || (mid - 1 >= 0 && nums[mid - 1] < target)) {
+                    return mid;
+                }
+                r = mid - 1;
+            }
+        }
+        return nums.length;
+    }
+}
