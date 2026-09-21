5cb15cb64dd3b2094c02074783552a07fedfacab
Aman Gupta <81767337+amanationn@users.noreply.github.com>
2026-09-21 23:15:23 +0530

    Add: intersection-of-two-linked-lists - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-linked-lists/submission-0.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Map<ListNode, Boolean> mp = new HashMap<>();
+        while(headA != null) {
+            mp.put(headA, true);
+            headA = headA.next;
+        }
+        while(headB != null) {
+            if(mp.containsKey(headB))
+                return headB;
+            headB = headB.next;
+        }
+        return null;
+    }
+}
