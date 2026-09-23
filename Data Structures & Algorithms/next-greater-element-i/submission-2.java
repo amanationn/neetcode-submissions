50d4d8c10af3e3c581ad778ccc6bd79b3d9add02
Aman Gupta <81767337+amanationn@users.noreply.github.com>
2026-09-23 16:08:39 +0530

    Add: next-greater-element-i - submission-2

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        Stack<Integer> stk = new Stack<>();
+        int n = nums2.length;
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        for(int i=n-1; i>=0; --i) {
+            int curr = nums2[i];
+
+            while(!stk.isEmpty() && stk.peek() <= curr) {
+                stk.pop();
+            }
+
+            if(stk.isEmpty()) {
+                mp.put(curr, -1);
+            }
+            else mp.put(curr, stk.peek());
+
+            stk.push(curr);
+        }
+
+        int m = nums1.length;
+        int[] ans = new int[m];
+        for(int i=0; i<m; ++i) {
+            ans[i] = mp.get(nums1[i]);
+        }
+        return ans;
+    }
+}
